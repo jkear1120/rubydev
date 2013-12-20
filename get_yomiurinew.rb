05e52cdfbe25affe8eda304538e78de654e60e3a
jkear1120 <jkear1120@gmail.com>
2013-12-20 16:29:14 +0900

    正規表現追加

--- get_yomiurinew.rb
+++ get_yomiurinew.rb
@@ -14,8 +14,9 @@ require 'net/https'
 #while true
 #サイト本文抽出
 begin
-	uri = "http://www.yomiuri.co.jp"
-	doc = Nokogiri::HTML(open(uri))
+	uri		=	"http://www.yomiuri.co.jp/latestnews"
+	base	=	"http://www.yomiuri.co.jp"
+	doc	=	Nokogiri::HTML(open(uri))
 	i=0
 	urls = Array.new
 	#リンクのみを指定
@@ -24,7 +25,7 @@ begin
 		#リンク判定(ローカルかグローバルか)
 		if %r{^/.*} =~ s
 			#文字列連結後配列へ
-			str = uri + s.to_s
+			str = base + s.to_s
 			#str.slice!(20)
 			urls.push(str)		
 			i+=1
@@ -36,7 +37,7 @@ begin
 	j=0
 	day = Time.now
 	daystr = day.strftime("%Y-%m-%d_%H:%M")
-	csvday = 'artcl-YomiuriTop' + daystr + '.csv'
+	csvday = 'articles' + daystr + '.csv'
 
 	#記事読み込み、CSVにめかぶ形式で保存
 	c = MeCab::Tagger.new(ARGV.join(" "))
@@ -44,12 +45,8 @@ begin
 		CSV.open(csvday,"a+", {:force_quotes => true}) do |csv|
 			arr1 = Nokogiri::HTML(open(urls[j]))
 			arr1.css("div.article-def").each do |node|
-				if /[\u618A]$/ =~ node.text
-					#csv << [c.parse(node.text)]
-					#puts node.text
-					deb = c.parse(node.text)
-					puts deb
-					break
+				if node =~ /^[・ぁ-ヴー一-龠]+$/u
+					csv << [c.parse(node.text)]
 				end
 			end
 		end
